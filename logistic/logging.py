fa66087385c5d9f71f50e1ffd154dc32fb93750f
yokogawadeveloper <ankul.gautam@yokogawa.com>
2024-03-14 10:54:37 +0530

    add loggers in application

---
+++ logistic/logging.py
@@ -0,0 +1,30 @@
+import logging.config
+import os
+
+from django.conf import settings
+
+# Define base directory
+BASE_DIR = settings.BASE_DIR
+
+# Define logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+# Apply the logging configuration
+logging.config.dictConfig(LOGGING)
